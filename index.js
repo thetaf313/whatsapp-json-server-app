d04440fdc2ff34aff66339d87122bfeffc86404a
Twist Price <tapha.ednayas313@gmail.com>
2025-06-10 23:39:10 +0000

    Updated index.js file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //
-import jsonServer from "json-server";
-
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
