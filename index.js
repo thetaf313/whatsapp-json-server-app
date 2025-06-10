81f1d8ad9ecccb957247a38ab1b34c1b3280ceed
Twist Price <tapha.ednayas313@gmail.com>
2025-06-10 23:41:16 +0000

    Updated index.js file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.port || 3001;
+const port = process.env.port || 8080;
 
 server.use(middlewares);
 server.use(router);
