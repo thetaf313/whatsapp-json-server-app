e539fb1a9bc25a733d99d8cc285d41f5adece731
Twist Price <tapha.ednayas313@gmail.com>
2025-06-13 12:00:48 +0000

    update cors policy

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.port || 8080;
 
+// Ajoute CORS headers
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // à sécuriser si besoin
+  res.header("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 server.use(middlewares);
 server.use(router);
 server.listen(port);
